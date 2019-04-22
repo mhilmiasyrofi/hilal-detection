169748680c1d3b4228a390d445c087bc07f83302
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2019-04-23 00:19:25 +0700

    create average image from video

--- video_extractor.py
+++ video_extractor.py
@@ -1,51 +1,62 @@
 import cv2
 import numpy as np
- 
-# Create a VideoCapture object
-cap = cv2.VideoCapture("video.avi")
- 
-# Check if camera opened successfully
-if (cap.isOpened() == False): 
-  print("Unable to read camera feed")
- 
-# Default resolutions of the frame are obtained.The default resolutions are system dependent.
-# We convert the resolutions from float to integer.
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
- 
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-# out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
 
-i = 0
+def convertFloat64ImgtoUint8(img):
+    # Get the information of the incoming image type
+    # normalize the img to 0 - 1
+    img = img.astype(np.float64) / float(img.max())
+    img = 255 * img  # Now scale by 255
+    img = img.astype(np.uint8)
+    return img
 
-while(True):
-  ret, frame = cap.read()
+if __name__ == "__main__":
 
-  i += 1
- 
-  if ret == True: 
-     
-    # Write the frame into the file 'output.avi'
-    # out.write(frame)
- 
-    # Display the resulting frame     
-    cv2.imshow('frame',frame)
-    print (type(frame))
-    if i == 30 :
-        wr = cv2.cvtColor(frame, cv2.COLOR_RGBA2RGB)
-        cv2.imwrite('frame.jpg',wr)
- 
-    # Press Q on keyboard to stop recording
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
- 
-  # Break the loop
-  else:
-    break 
- 
-# When everything done, release the video capture and video write objects
-cap.release()
-out.release()
- 
-# Closes all the frames
-cv2.destroyAllWindows() 
+    # Create a VideoCapture object
+    filename = "data/Video Hilal/Data1/Flat/10_39_12.avi"
+    cap = cv2.VideoCapture(filename)
+
+    # Check if camera opened successfully
+    if (cap.isOpened() == False):
+        print("Unable to read camera feed")
+
+    # Default resolutions of the frame are obtained.The default resolutions are system dependent.
+    # We convert the resolutions from float to integer.
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+  
+    i = 0
+
+    sum_images = None
+    while(True):
+        ret, frame = cap.read()
+
+        if ret == True:
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
+            # print (type(frame))
+            print(i)
+            if i == 0:
+                # print(frame)
+                sum_images = frame.astype(np.float64)
+            else:
+                sum_images += frame
+            i += 1
+
+            # Press Q on keyboard to stop recording
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Break the loop
+        else:
+            break
+
+    # When everything done, release the video capture and video write objects
+    cap.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+    normalized = sum_images/i
+    normalized_image = normalized.astype(np.uint8)
+    
+    # cv2.imwrite('frame.jpg', normalized_image)
